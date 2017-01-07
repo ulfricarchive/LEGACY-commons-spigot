be506db7b807f55715e5f3e8b8c9361e7b78cc3b
Adam Harrison <ad@mharrison.us>
2017-01-07 18:40:33 -0500

    Adds @Broadcast

---
+++ src/main/java/com/ulfric/commons/spigot/intercept/server/Broadcast.java
@@ -0,0 +1,19 @@
+package com.ulfric.commons.spigot.intercept.server;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.ulfric.commons.cdi.intercept.Intercept;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
+@Intercept
+public @interface Broadcast {
+
+	String requiredPermission() default "";
+
+}
--- src/main/java/com/ulfric/commons/spigot/plugin/UlfricPlugin.java
+++ src/main/java/com/ulfric/commons/spigot/plugin/UlfricPlugin.java
@@ -12,6 +12,8 @@ import com.ulfric.commons.spigot.intercept.player.MustBeAlive;
 import com.ulfric.commons.spigot.intercept.player.MustBeAliveInterceptor;
 import com.ulfric.commons.spigot.intercept.player.MustBeDead;
 import com.ulfric.commons.spigot.intercept.player.MustBeDeadInterceptor;
+import com.ulfric.commons.spigot.intercept.server.Broadcast;
+import com.ulfric.commons.spigot.intercept.server.BroadcastInterceptor;
 import com.ulfric.commons.spigot.logging.ConsoleLogger;
 import com.ulfric.commons.spigot.module.Disable;
 import com.ulfric.commons.spigot.module.DisableInterceptor;
@@ -86,6 +88,8 @@ public abstract class UlfricPlugin extends JavaPlugin implements Named {
 		factory.bind(Disable.class).toInterceptor(DisableInterceptor.class);
 
 		factory.bind(OnMainThread.class).toInterceptor(OnMainThreadInterceptor.class);
+
+		factory.bind(Broadcast.class).toInterceptor(BroadcastInterceptor.class);
 
 		factory.bind(RequirePermission.class).toInterceptor(RequirePermissionInterceptor.class);
 		factory.bind(RequirePermissions.class).toInterceptor(RequirePermissionsInterceptor.class);
