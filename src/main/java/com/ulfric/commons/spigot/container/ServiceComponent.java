28dc352a6fcd7978b6d6e168ebd325a422576f34
Adam Harrison <ad@mharrison.us>
2017-01-22 20:31:26 -0500

    fetch service type from implemented interfaces

--- src/main/java/com/ulfric/commons/spigot/container/ServiceComponent.java
+++ src/main/java/com/ulfric/commons/spigot/container/ServiceComponent.java
@@ -3,19 +3,35 @@ package com.ulfric.commons.spigot.container;
 import com.ulfric.commons.cdi.container.Component;
 import com.ulfric.commons.cdi.container.SkeletalComponent;
 import com.ulfric.commons.service.Service;
+import com.ulfric.commons.spigot.cdi.scope.service.NotAServiceException;
 import com.ulfric.commons.spigot.service.ServiceUtils;
 
 public final class ServiceComponent<S extends Service> extends SkeletalComponent {
 
-	public ServiceComponent(Component parent, Class<S> service, S implementation)
-	{
-		super(parent);
-		this.service = service;
-		this.implementation = implementation;
-	}
-
 	private final Class<S> service;
 	private final S implementation;
+
+	public ServiceComponent(Component parent, S implementation)
+	{
+		super(parent);
+		this.implementation = implementation;
+		this.service = this.getServiceFromInterfaces();
+	}
+
+	private Class<S> getServiceFromInterfaces()
+	{
+		for (Class<?> iface : this.implementation.getClass().getInterfaces())
+		{
+			if (Service.class.isAssignableFrom(iface))
+			{
+				@SuppressWarnings("unchecked")
+				Class<S> service = (Class<S>) iface;
+				return service;
+			}
+		}
+
+		throw new NotAServiceException();
+	}
 
 	@Override
 	public void enable()
