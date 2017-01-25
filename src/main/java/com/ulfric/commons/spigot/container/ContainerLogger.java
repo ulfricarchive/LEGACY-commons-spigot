7153cd5122114d7816783cda87b1119375252eec
insou22 <zac.kologlu@gmail.com>
2017-01-25 13:40:26 +1100

    Be more A B S T R A C T

--- src/main/java/com/ulfric/commons/spigot/container/ContainerLogger.java
+++ src/main/java/com/ulfric/commons/spigot/container/ContainerLogger.java
@@ -2,13 +2,14 @@ package com.ulfric.commons.spigot.container;
 
 import java.util.logging.Logger;
 
+import org.bukkit.plugin.Plugin;
+
 import com.ulfric.commons.cdi.scope.Supplied;
-import com.ulfric.commons.spigot.plugin.UlfricPlugin;
 
 @Supplied
 public final class ContainerLogger extends Logger {
 
-	public ContainerLogger(UlfricPlugin plugin, Logger parent)
+	public ContainerLogger(Plugin plugin, Logger parent)
 	{
 		super(plugin.getName(), parent.getResourceBundleName());
 	}
