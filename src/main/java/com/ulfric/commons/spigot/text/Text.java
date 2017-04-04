82b97ff992b087625a2a48f4938009d97dbd12ab
Adam Harrison <ad@mharrison.us>
2017-04-04 13:02:34 -0400

    Add service details to Text

--- src/main/java/com/ulfric/commons/spigot/text/Text.java
+++ src/main/java/com/ulfric/commons/spigot/text/Text.java
@@ -2,8 +2,12 @@ package com.ulfric.commons.spigot.text;
 
 import org.bukkit.command.CommandSender;
 
+import com.ulfric.commons.naming.Name;
 import com.ulfric.commons.service.Service;
+import com.ulfric.commons.version.Version;
 
+@Name("Text")
+@Version(1)
 public interface Text extends Service {
 
 	String getMessage(CommandSender target, String code);
