a88fcd71c59447919c060f9da5a062f3bb084720
insou22 <zac.kologlu@gmail.com>
2017-04-11 03:48:47 +1000

    This hurts

--- src/main/java/com/ulfric/commons/spigot/metadata/Metadata.java
+++ src/main/java/com/ulfric/commons/spigot/metadata/Metadata.java
@@ -1,31 +1,47 @@
 package com.ulfric.commons.spigot.metadata;
 
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
-import org.bukkit.metadata.Metadatable;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public enum Metadata {
 
 	;
 
-	private static final Map<Object, MetadataHolder> METADATA = new HashMap<>();
+	private static final Map<Class<?>, Map<String, MetadataHolder>> METADATA = new IdentityHashMap<>();
 
-	public static void write(Object holder, String key, Object value)
+	public static void write(Player holder, String key, Object value)
 	{
 		MetadataHolder data = Metadata.getOrCreateHolder(holder);
 
 		data.set(key, value);
 	}
 
-	public static <T> Object read(T holder, String key)
+	public static void write(CommandSender holder, String key, Object value)
+	{
+		MetadataHolder data = Metadata.getOrCreateHolder(holder);
+
+		data.set(key, value);
+	}
+
+	public static Object read(Player holder, String key)
 	{
 		MetadataHolder data = Metadata.getOrCreateHolder(holder);
 
 		return data.get(key);
 	}
 
-	public static String readString(Metadatable holder, String key)
+	public static Object read(CommandSender holder, String key)
+	{
+		MetadataHolder data = Metadata.getOrCreateHolder(holder);
+
+		return data.get(key);
+	}
+
+	public static String readString(Player holder, String key)
 	{
 		Object value = Metadata.read(holder, key);
 
@@ -37,9 +53,32 @@ public enum Metadata {
 		return null;
 	}
 
-	private static MetadataHolder getOrCreateHolder(Object holder)
+	public static String readString(CommandSender holder, String key)
 	{
-		return Metadata.METADATA.computeIfAbsent(holder, ignored -> new MetadataHolder());
+		Object value = Metadata.read(holder, key);
+
+		if (value instanceof String)
+		{
+			return (String) value;
+		}
+
+		return null;
+	}
+
+	private static MetadataHolder getOrCreateHolder(Player holder)
+	{
+		Map<String, MetadataHolder> metadatables =
+				Metadata.METADATA.computeIfAbsent(holder.getClass(), ignored -> new HashMap<>());
+
+		return metadatables.computeIfAbsent(holder.getUniqueId().toString(), ignored -> new MetadataHolder());
+	}
+
+	private static MetadataHolder getOrCreateHolder(CommandSender holder)
+	{
+		Map<String, MetadataHolder> metadatables =
+				Metadata.METADATA.computeIfAbsent(holder.getClass(), ignored -> new HashMap<>());
+
+		return metadatables.computeIfAbsent(holder.getName(), ignored -> new MetadataHolder());
 	}
 
 }
