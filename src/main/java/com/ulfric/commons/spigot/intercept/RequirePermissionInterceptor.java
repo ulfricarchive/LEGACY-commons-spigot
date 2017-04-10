49ac6f9025c9369428bb582023c45938fefe5a8a
insou22 <zac.kologlu@gmail.com>
2017-04-11 01:21:08 +1000

    More RequirePermissionInterceptor improvements!

--- src/main/java/com/ulfric/commons/spigot/intercept/RequirePermissionInterceptor.java
+++ src/main/java/com/ulfric/commons/spigot/intercept/RequirePermissionInterceptor.java
@@ -1,10 +1,17 @@
 package com.ulfric.commons.spigot.intercept;
 
+import java.lang.reflect.Field;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerEvent;
 
+import com.ulfric.commons.exception.Try;
 import com.ulfric.commons.spigot.metadata.Metadata;
 import com.ulfric.commons.spigot.metadata.MetadataDefaults;
 import com.ulfric.commons.spigot.text.Text;
@@ -12,6 +19,8 @@ import com.ulfric.dragoon.intercept.Context;
 import com.ulfric.dragoon.intercept.Interceptor;
 
 public class RequirePermissionInterceptor implements Interceptor {
+
+	private static final Map<Class<?>, List<Field>> PLAYER_FIELDS = new IdentityHashMap<>();
 
 	private final Text text = Text.getService();
 
@@ -55,12 +64,31 @@ public class RequirePermissionInterceptor implements Interceptor {
 			return (Player) argument;
 		}
 
-		if (argument instanceof PlayerEvent)
-		{
-			return ((PlayerEvent) argument).getPlayer();
-		}
+		return RequirePermissionInterceptor.PLAYER_FIELDS.computeIfAbsent(argument.getClass(), this::findPlayerFields)
+				.stream()
+				.map(field -> (Player) this.getFieldValue(field, argument))
+				.filter(Objects::nonNull)
+				.findFirst()
+				.orElse(null);
+	}
 
-		return null;
+	private List<Field> findPlayerFields(Class<?> clazz)
+	{
+		return FieldUtils.getAllFieldsList(clazz)
+				.stream()
+				.filter(this::fieldTypeIsPlayer)
+				.peek(field -> field.setAccessible(true))
+				.collect(Collectors.toList());
+	}
+
+	private boolean fieldTypeIsPlayer(Field field)
+	{
+		return Player.class.isAssignableFrom(field.getType());
+	}
+
+	private Object getFieldValue(Field field, Object instance)
+	{
+		return Try.to(() -> field.get(instance));
 	}
 
 }
