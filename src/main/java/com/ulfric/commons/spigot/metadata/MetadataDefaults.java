90638e266d5bded0b34cddbfb61310af317b3aea
Adam Harrison <ad@mharrison.us>
2017-04-16 00:35:12 -0400

    More metadata

--- src/main/java/com/ulfric/commons/spigot/metadata/MetadataDefaults.java
+++ src/main/java/com/ulfric/commons/spigot/metadata/MetadataDefaults.java
@@ -5,6 +5,7 @@ public class MetadataDefaults {
 	public static final String LAST_TARGETED_BY = "LastTargetedBy";
 	public static final String LAST_PERMISSION_TOUCH = "LastPermissionTouch";
 	public static final String LAST_PERMISSION_TOUCH_NODE = "LastPermissionTouchNode";
+	public static final String LAST_PERMISSION_TOUCH_PARENT = "LastPermissionTouchParent";
 	public static final String NO_PERMISSION = "NoPermission";
 
 	private MetadataDefaults()
