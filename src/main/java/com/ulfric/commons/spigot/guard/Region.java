bac43529ed569803bd5d982572f77e647409032a
Adam Harrison <ad@mharrison.us>
2017-04-21 02:41:36 -0400

    Global region API

--- src/main/java/com/ulfric/commons/spigot/guard/Region.java
+++ src/main/java/com/ulfric/commons/spigot/guard/Region.java
@@ -24,6 +24,7 @@ public final class Region extends Bean implements Named, Weighted<Region> {
 		private Shape bounds;
 		private int weight;
 		private Map<Flag<?>, Object> flags;
+		private boolean global;
 
 		Builder() { }
 
@@ -33,7 +34,7 @@ public final class Region extends Bean implements Named, Weighted<Region> {
 			Objects.requireNonNull(this.name, "name");
 			Objects.requireNonNull(this.world, "world");
 			Map<Flag<?>, Object> flags = this.flags == null ? Collections.emptyMap() : new HashMap<>(this.flags);
-			return new Region(this.name, this.world, this.bounds, this.weight, flags);
+			return new Region(this.name, this.world, this.bounds, this.weight, flags, this.global);
 		}
 
 		public Builder setName(String name)
@@ -65,6 +66,12 @@ public final class Region extends Bean implements Named, Weighted<Region> {
 			this.flags = flags;
 			return this;
 		}
+
+		public Builder setGlobal(boolean global)
+		{
+			this.global = global;
+			return this;
+		}
 	}
 
 	private final String name;
@@ -72,14 +79,16 @@ public final class Region extends Bean implements Named, Weighted<Region> {
 	private final Shape bounds;
 	private final int weight;
 	private final Map<Flag<?>, Object> flags;
+	private final boolean global;
 
-	private Region(String name, UUID world, Shape bounds, int weight, Map<Flag<?>, Object> flags)
+	private Region(String name, UUID world, Shape bounds, int weight, Map<Flag<?>, Object> flags, boolean global)
 	{
 		this.name = name;
 		this.world = world;
 		this.bounds = bounds;
 		this.weight = weight;
 		this.flags = flags;
+		this.global = global;
 	}
 
 	@Override
@@ -102,6 +111,11 @@ public final class Region extends Bean implements Named, Weighted<Region> {
 	public int getWeight()
 	{
 		return this.weight;
+	}
+
+	public boolean isGlobal()
+	{
+		return this.global;
 	}
 
 	@SuppressWarnings("unchecked")
