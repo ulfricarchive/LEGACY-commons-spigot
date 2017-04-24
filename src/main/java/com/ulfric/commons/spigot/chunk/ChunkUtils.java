99fcc6f7533fb3448eb34cdbec64f16e82d1899d
BuildTools <cjpet10@gmail.com>
2017-04-24 16:02:33 -0400

    removed header and fixed chunk utils

--- src/main/java/com/ulfric/commons/spigot/chunk/ChunkUtils.java
+++ src/main/java/com/ulfric/commons/spigot/chunk/ChunkUtils.java
@@ -6,24 +6,23 @@ import net.minecraft.server.v1_11_R1.ChunkSection;
 import net.minecraft.server.v1_11_R1.IBlockData;
 import org.bukkit.Material;
 
-/**
- * Created by Connor on 4/24/2017.
- */
-public class ChunkUtils {
+public enum ChunkUtils {
 
-  public IBlockData nmsBlock(Material material, byte data){
+  ;
+
+  public static IBlockData nmsBlock(Material material, byte data) {
     int id = material.getId();
     int combined = id + (data << 12);
     return net.minecraft.server.v1_11_R1.Block.getByCombinedId(combined);
   }
 
-  public void setBlockInChunk(Chunk chunk, int x, int y, int z, IBlockData iBlockData){
-    chunk.a(new BlockPosition(x,y,z), iBlockData);
+  public static void setBlockInChunk(Chunk chunk, int x, int y, int z, IBlockData iBlockData) {
+    chunk.a(new BlockPosition(x, y, z), iBlockData);
   }
 
-  public void applyChanges(Chunk chunk, ChunkSection[] sections){
-    System.arraycopy(sections,0,chunk.getSections(),0,sections.length);
-    chunk.world.getWorld().refreshChunk(chunk.bukkitChunk.getX(),chunk.bukkitChunk.getZ());
+  public static void applyChanges(Chunk chunk, ChunkSection[] sections) {
+    System.arraycopy(sections, 0, chunk.getSections(), 0, sections.length);
+    chunk.world.getWorld().refreshChunk(chunk.bukkitChunk.getX(), chunk.bukkitChunk.getZ());
   }
 
 }
