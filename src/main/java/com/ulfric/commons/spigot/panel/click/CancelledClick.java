57ad9cc6a185ca91c3c1f1a4af9323007961cfb8
Adam Harrison <ad@mharrison.us>
2017-04-30 20:14:28 -0400

    Add missing @Override annotation

--- src/main/java/com/ulfric/commons/spigot/panel/click/CancelledClick.java
+++ src/main/java/com/ulfric/commons/spigot/panel/click/CancelledClick.java
@@ -3,6 +3,7 @@ package com.ulfric.commons.spigot.panel.click;
 @FunctionalInterface
 public interface CancelledClick<T extends ClickData> extends Click<T> {
 
+	@Override
 	default ClickResult handle(T click)
 	{
 		this.handleCancelled(click);
