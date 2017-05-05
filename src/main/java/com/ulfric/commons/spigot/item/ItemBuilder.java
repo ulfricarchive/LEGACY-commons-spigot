0a03d0be2333986e8279296d4bca66e4b2d3d6b3
insou22 <zac.kologlu@gmail.com>
2017-05-05 12:05:58 +1000

    Refactor ItemBuilder

--- src/main/java/com/ulfric/commons/spigot/item/ItemBuilder.java
+++ src/main/java/com/ulfric/commons/spigot/item/ItemBuilder.java
@@ -5,50 +5,52 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.Builder;
+import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import com.ulfric.commons.spigot.text.ChatUtils;
+import com.ulfric.commons.spigot.text.Text;
 
 public class ItemBuilder implements Builder<ItemStack> {
 
 	private MaterialType type;
-	private int amount;
+	private int amount = 1;
 	private String displayName;
 	private List<String> lore;
-	private boolean colorize;
+	private CommandSender formatFor;
 	private Map<Enchantment, Integer> enchantments;
 
 	@Override
 	public ItemStack build()
 	{
-		Objects.requireNonNull(type, "type");
+		Objects.requireNonNull(this.type, "type");
 		
 		MaterialType type = this.type;
 		
-		ItemStack itemStack = new ItemStack(type.getType(), this.amount == 0 ? 1 : this.amount, type.getData());
+		ItemStack itemStack = new ItemStack(type.getType(), this.amount, type.getData());
 		
 		ItemMeta meta = itemStack.getItemMeta();
+
+		Function<String, String> plainMessage = this.plainMessageProvider();
 		
 		if (this.displayName != null)
 		{
-			meta.setDisplayName(this.colorize ? ChatUtils.format(this.displayName) : this.displayName);
+			meta.setDisplayName(plainMessage.apply(this.displayName));
 		}
 		
-		if (this.lore != null && !this.lore.isEmpty())
+		if (this.lore != null)
 		{
-			if (this.colorize)
-			{
-				meta.setLore(new ArrayList<>(this.lore).stream().map(ChatUtils::format).collect(Collectors.toList()));
-			}
-			else
-			{
-				meta.setLore(this.lore);
-			}
+			meta.setLore(
+					new ArrayList<>(this.lore)
+							.stream()
+							.map(plainMessage)
+							.collect(Collectors.toList())
+			);
 		}
 		
 		itemStack.addEnchantments(this.enchantments);
@@ -56,27 +58,41 @@ public class ItemBuilder implements Builder<ItemStack> {
 		return itemStack;
 	}
 
+	private Function<String, String> plainMessageProvider()
+	{
+		if (this.formatFor != null)
+		{
+			return code -> Text.getService().getPlainMessage(this.formatFor, code);
+		}
+
+		return code -> Text.getService().getPlainMessage(code);
+	}
+
 	public ItemBuilder setMaterialType(MaterialType type)
 	{
 		this.type = type;
+
 		return this;
 	}
 
 	public ItemBuilder setAmount(int amount)
 	{
 		this.amount = amount;
+
 		return this;
 	}
 
 	public ItemBuilder setDisplayName(String displayName)
 	{
 		this.displayName = displayName;
+
 		return this;
 	}
 
 	public ItemBuilder setLore(List<String> lore)
 	{
 		this.lore = lore;
+
 		return this;
 	}
 
@@ -85,15 +101,17 @@ public class ItemBuilder implements Builder<ItemStack> {
 		return this.setLore(Arrays.asList(lore));
 	}
 
-	public ItemBuilder colorize()
-	{
-		this.colorize = true;
-		return this;
-	}
-
 	public ItemBuilder addEnchantment(Enchantment enchantment, int level)
 	{
 		this.enchantments.put(enchantment, level);
+
+		return this;
+	}
+
+	public ItemBuilder formatFor(CommandSender sender)
+	{
+		this.formatFor = sender;
+
 		return this;
 	}
 
