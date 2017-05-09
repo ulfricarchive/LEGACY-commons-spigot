545bd5be6af655c3171acd8ee41c6ebe732a95d5
Adam Harrison <ad@mharrison.us>
2017-05-09 02:18:18 -0400

    Tighten file name restrictions on warps

--- src/main/java/com/ulfric/commons/spigot/warp/Warp.java
+++ src/main/java/com/ulfric/commons/spigot/warp/Warp.java
@@ -1,15 +1,21 @@
 package com.ulfric.commons.spigot.warp;
 
-import com.ulfric.commons.bean.Bean;
-
 import java.util.Objects;
 import java.util.regex.Pattern;
 
 import org.bukkit.Location;
 
-public final class Warp extends Bean {
+import com.ulfric.commons.bean.Bean;
+import com.ulfric.commons.naming.Named;
 
-	public static final Pattern WARP_NAME = Pattern.compile("[^\\s]+");
+public final class Warp extends Bean implements Named {
+
+	private static final Pattern VALID_WARP_NAME = Pattern.compile("[a-zA-Z0-9_-]+");
+
+	public static boolean isValidName(String name)
+	{
+		return Warp.VALID_WARP_NAME.matcher(name).matches();
+	}
 
 	public static Builder builder()
 	{
@@ -29,9 +35,9 @@ public final class Warp extends Bean {
 			Objects.requireNonNull(this.name, "name");
 			Objects.requireNonNull(this.location, "location");
 
-			if (!Warp.WARP_NAME.matcher(this.name).matches())
+			if (!Warp.isValidName(this.name))
 			{
-				throw new IllegalArgumentException(this.name + " must match " + Warp.WARP_NAME.pattern());
+				throw new IllegalArgumentException(this.name + " must match " + Warp.VALID_WARP_NAME.pattern());
 			}
 
 			return new Warp(this.name, this.location);
@@ -59,6 +65,7 @@ public final class Warp extends Bean {
 		this.location = location;
 	}
 
+	@Override
 	public String getName()
 	{
 		return this.name;
